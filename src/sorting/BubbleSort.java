0503e06277dd60fd129182d7c066713e74abd139
apzal <apzal.bn@gmail.com>
2024-08-19 10:29:44 +0530

    sorting new commit

---
+++ src/sorting/BubbleSort.java
@@ -0,0 +1,11 @@
+package sorting;
+
+public class BubbleSort {
+
+    /*
+    [7,8,2,1,6,5]
+
+    for j in range (0,arr(length))
+
+     */
+}
---
+++ src/sorting/SelectionSort.java
@@ -0,0 +1,23 @@
+package sorting;
+
+public class SelectionSort {
+
+    /*
+      [3,1,2,5,4] => [1,3,2,5,4] => [1,2,3,5,4]
+      [1,2,3,5,4] => [1,2,3,4,5]
+
+      input: array A
+       0       n-1
+      [3,1,2,5,4]
+           ^
+           i
+      for i in range (0,n-1)
+          minIndex = i;
+          for i in range(i+1,n-1)
+            if A[i] < A[minIndex]
+               minIndex = i;
+          end
+             swap(A[i],A[minIndex])
+      end
+     */
+}
